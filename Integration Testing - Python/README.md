9b3d9f4344b693a8bf2de99a0725d04b4ea3d697
Dalton Pruitt <dp987@msstate.edu>
2020-04-11 17:10:01 -0500

    Update SeleniumBrowserTesting.py; auxillary files added
    Updated the Selenium-based tests; now works for the Sign up and Login pages. Added some of the Selenium IDE (SIDE) files. Attempt to add a log for test results.

---
+++ Integration Testing - Python/README.md
@@ -0,0 +1,10 @@
+#Instructions for Using Python Selenium Test
+
+1. Be sure the (Selenium package)[https://pypi.org/project/selenium/] is on your computer (and can be reached by python)
+2. Start XAMPP and run the database.py file
+4. Into a text document, copy the path to the local repository you are in (it helps with logging later)
+5. Run the "Selenium Browser Testing.py" file
+
+For testing with FireFox:
+- Download (geckdriver)[https://github.com/mozilla/geckodriver/releases]
+- (Add the geckodriver executable to PATH)[https://stackoverflow.com/questions/40208051/selenium-using-python-geckodriver-executable-needs-to-be-in-path]
--- IntegrationTestingPython/SeleniumBrowserTesting.py
+++ IntegrationTestingPython/SeleniumBrowserTesting.py
@@ -8,6 +8,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from os import startfile
 
+from signup_test import TestAchieversSite
+
+'''
 use_def = input("Use defaults? (y/n)").lower()
 yeses = ["yes", "y", 'ye']
 nos = ["no","n"]
@@ -19,42 +22,23 @@ while use_def not in yeses + nos:
 if use_def in yeses:
     repoDirectory = "C:\\xampp\\htdocs\\Achievers"
     xamppLinkAddress = repoDirectory + "\\IntegrationTestingPython\\xampp-control.lnk"
+    try:
+        startfile(xamppLinkAddress)
+    except Exception as e:
+        print("Error:", e, " Which really means the xampp stuff couldn't be started.")
 
 elif use_def in nos:
     repoDirectory = input("Please enter (copy/paste) the repository directory")
     wait = input("Please start the database, then press Enter.")
     if wait is not None:
         wait = None
-
-try:
-    startfile(xamppLinkAddress)
-except Exception as e:
-    print("Error:", e, " Which really means the xampp stuff couldn't be started.")
-
-
+'''
 
 driver = webdriver.Firefox()
-driver.get("http://localhost/Achievers/Signup.html")
-assert "signup" in driver.title
-
-entries = (("username", "Test User"), ("email", "TestEmail@email.com"),
-           ("password", "Test Password"), ("password", "Test Password") )
-elem = driver.find_element_by_name("username")
-elem.clear()
-elem.send_keys("Test User")
-elem = driver.find_element_by_name("username")
-elem = elem.send_keys("TestEmail@email.com")
-driver.find_element_by_name(Keys.TAB)
-elem.send_keys("TestPassword")
-driver.find_element_by_name(Keys.TAB)
-elem.send_keys("TestPassword")
-driver.find_element_by_name(Keys.TAB)
-elem.send_keys(Keys.RETURN)
-
-assert "No results found." not in driver.page_source
-driver.close()
-
-'''
+try:
+    test = TestAchieversSite()
+    test.run(driver=driver) # actually running the tests
 except Exception as e:
-    print(e)'''
-
+    print(e)
+finally:
+    test.teardown_method()
